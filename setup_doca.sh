ce4f0c4026331a27268401203504aedd36f9014e
Xiaolong Huang <xiaoloonghuang@gmail.com>
2024-12-01 00:31:10 -0700

    update doca

--- setup_doca.sh
+++ setup_doca.sh
@@ -3,12 +3,19 @@
 cd $HOME
 
 . /etc/os-release
-if [[ $VERSION_ID != "20.04" ]]; then
-    echo "This script only support ubuntu 20.04"
+
+if [[ $VERSION_ID == "20.04" ]]; then
+    wget https://www.mellanox.com/downloads/DOCA/DOCA_v2.9.0/host/doca-host_2.9.0-129000-24.10-ubuntu2004_amd64.deb
+    sudo dpkg -i doca-host_2.9.0-129000-24.10-ubuntu2004_amd64.deb
+elif [[ $VERSION_ID == "24.04" ]]; then
+    wget https://www.mellanox.com/downloads/DOCA/DOCA_v2.9.0/host/doca-host_2.9.0-129000-24.10-ubuntu2404_amd64.deb
+    sudo dpkg -i doca-host_2.9.0-129000-24.10-ubuntu2404_amd64.deb
+else
+    echo "Unsupported OS version"
     exit 1
 fi
 
-wget https://www.mellanox.com/downloads/DOCA/DOCA_v2.9.0/host/doca-host_2.9.0-129000-24.10-ubuntu2004_amd64.deb
-sudo dpkg -i doca-host_2.9.0-129000-24.10-ubuntu2004_amd64.deb
 sudo apt-get update
-sudo apt-get -y install doca-all
+if [[ -n $1 ]]; then
+    sudo apt-get -y install $1
+fi
