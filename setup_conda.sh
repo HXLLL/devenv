0f7b7d1b28919bf37ab7fb4b7b065b8b78092936
Xiaolong Huang <xiaoloonghuang@gmail.com>
2024-12-01 03:07:07 -0700

    update conda

--- setup_conda.sh
+++ setup_conda.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O ~/miniconda.sh
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
 bash ~/miniconda.sh -b -p $HOME/miniconda
 source $HOME/miniconda/bin/activate
 conda create -n default python=3.13
