aafad54f1bfe742c4754e80eb77756eb9631507d
Xiaolong Huang <xiaoloonghuang@gmail.com>
2024-12-01 03:07:30 -0700

    update

--- setup_conda.sh
+++ setup_conda.sh
@@ -3,6 +3,6 @@
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
 bash ~/miniconda.sh -b -p $HOME/miniconda
 source $HOME/miniconda/bin/activate
-conda create -n default python=3.13
+conda create -y -n default python=3.13
 conda activate default
 conda install -y -c conda-forge fire ipykernel matplotlib numpy pandas
