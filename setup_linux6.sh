7491a9ddb13617d7020cd1e5e4f2710e927b57e7
Xiaolong Huang <xiaoloonghuang@gmail.com>
2024-12-21 03:29:31 -0700

    update

---
+++ setup_linux6.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cp dotfiles/.config $HOME/kernel_config
+cd $HOME
+
+git clone https://github.com/HXLLL/kernel-patched linux6
+
+cd kernel_patched
+git checkout linux6-hxl
+mv $HOME/kernel_config .config
+
+make bzImage -j$(nproc)
+make modules -j$(nproc)
+sudo make modules_install
+sudo make install
